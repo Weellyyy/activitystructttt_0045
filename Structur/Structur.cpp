2109b768193163366cbbd68812c20c696d9824b7
Weellyyy <wildanprtm2304@gmail.com>
2024-04-02 18:18:46 +0700

    menambahkan int main

--- Structur/Structur.cpp
+++ Structur/Structur.cpp
@@ -7,3 +7,15 @@ struct mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+}
